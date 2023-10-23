2ff62e32179594bb248cd72b4a895c336f6bc6d2
Bahriddin <Bahriddin>
2023-10-24 00:20:45 +0500

    4dars

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+confirm("browserda ishlash mumkuni");
+let fname=prompt("isming nims?");
+let student = prompt("qayerda o'qiysan ?");
+let age = prompt("yoshing nechida ?");
+let hommi = prompt("kim bo'lmoqchisan?");
+let ismarret = prompt("Uylanganmisan?");
+let marret = prompt("nechta farzanding bor")
+console.log("mening ismim" +" " + fname + " ", "men" + " " +  student + "da o'qiyman", "yoshim"  + " " + age + " "+ "da", "men" + " " + hommi + " " + "bo'lmoqciman" + "men xali" + ismarret + " lekin" + marret +"bor" );
